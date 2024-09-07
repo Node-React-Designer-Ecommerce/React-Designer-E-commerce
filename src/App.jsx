993688187a445cfb9f578afc0588e284d95e6107
yassersalem14 <salemyasserlallo1415@gmail.com>
2024-09-08 01:51:27 +0200

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <div className="flex flex-col items-center justify-center bg-green-500 gap-5  text-3xl font-bold p-5">
+      <div className="">Final Project ! </div>
+      <div className="">إن شاء الله خير</div>
+      <div className="">إن شاء الله خير</div>
+      <div className="">إن شاء الله خير</div>
+    </div>
+
+  )
+}
+
+export default App
