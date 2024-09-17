10b1cda341526d5b132e08d8be35efb1aaaea48f
Zahra Abdelaleem <123109918+zahra3leem@users.noreply.github.com>
2024-09-17 21:11:55 +0300

    logo to7fa feature/landing-page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
-import  AuthContext  from '../context/AuthContext';
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "../context/AuthContext";
 
 export default function Navbar() {
   const { isLoggedIn, logout } = useContext(AuthContext);
@@ -8,28 +8,48 @@ export default function Navbar() {
 
   const handleLogout = () => {
     logout();
-    navigate('/login');
+    navigate("/login");
   };
 
   return (
     <div className="">
       <div className="navbar bg-base-100 border-b-2">
         <div className="flex-1">
-          <Link to="/" className=''>
-            <img src="/drying-clothes-3d-icon-png.webp" width={100} alt="logo" />
+          <Link to="/" className="">
+            <img src="smm.png" width={150} alt="logo" />
           </Link>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
-          <Link to="/products" className='px-5 py-1 font-bold text-sky-500 text-xl  hover:text-sky-400 hover:transition-all'>products</Link>
+            <Link
+              to="/products"
+              className="px-5 py-1 font-bold text-sky-500 text-xl  hover:text-sky-400 hover:transition-all"
+            >
+              products
+            </Link>
             {!isLoggedIn && (
-              <Link to="/login" className='px-5 py-1 font-bold text-sky-500 text-xl  hover:text-sky-400 hover:transition-all'>Login</Link>
+              <Link
+                to="/login"
+                className="px-5 py-1 font-bold text-sky-500 text-xl  hover:text-sky-400 hover:transition-all"
+              >
+                Login
+              </Link>
             )}
             {!isLoggedIn && (
-              <Link to="/sign-up" className='px-5 py-1 font-bold text-sky-500 text-xl  hover:text-sky-400 hover:transition-all'>Register</Link>
+              <Link
+                to="/sign-up"
+                className="px-5 py-1 font-bold text-sky-500 text-xl  hover:text-sky-400 hover:transition-all"
+              >
+                Register
+              </Link>
             )}
             {isLoggedIn && (
-              <button onClick={handleLogout} className='px-5 py-3 text-lg bg-red-600 text-white rounded-lg font-bold hover:bg-red-500 hover:transition-all'>Logout</button>
+              <button
+                onClick={handleLogout}
+                className="px-5 py-3 text-lg bg-red-600 text-white rounded-lg font-bold hover:bg-red-500 hover:transition-all"
+              >
+                Logout
+              </button>
             )}
           </ul>
         </div>
