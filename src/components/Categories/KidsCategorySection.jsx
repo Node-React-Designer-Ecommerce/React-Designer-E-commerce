b8b9540ef4614a086db6e2425b447735b971d39e
Zahra Abdelaleem <123109918+zahra3leem@users.noreply.github.com>
2024-09-18 15:19:58 +0300

    feature/add image from api and change some paths

--- src/components/Categories/KidsCategorySection.jsx
+++ src/components/Categories/KidsCategorySection.jsx
@@ -1,11 +1,12 @@
 
+import { Link } from 'react-router-dom';
 function KidsCategorySection() {
   return (
     <div>
           <div className="relative">
             <img src="kids.jpg"alt="Kids" className="w-full object-cover" />
             <div className="absolute top-4 left-6">
-              <button className="bg-SecondaryColor text-white px-4 py-2">KIDS</button>
+              <Link to="/products" className="bg-SecondaryColor text-white px-4 py-2">KIDS</Link>
             </div>
           </div>
     </div>
--- src/components/Categories/MenCategorySection.jsx
+++ src/components/Categories/MenCategorySection.jsx
@@ -1,11 +1,12 @@
 
+import { Link } from 'react-router-dom';
 function MenCategorySection() {
   return (
     <div>
           <div className="relative">
             <img src="men.jpg" alt="Men" className="w-full h-auto object-cover" />
             <div className="absolute bottom-4 left-4">
-              <button className="bg-SecondaryColor text-white  px-4 py-2">MEN</button>
+              <Link to="/products" className="bg-SecondaryColor text-white  px-4 py-2">MEN</Link>
             </div>
           </div>
     </div>
