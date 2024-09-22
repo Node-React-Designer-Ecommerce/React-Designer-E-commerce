1f230de633d5e88fb0e2b64f54583dd9bbf87ede
shadyyd <yd.radwan@gmail.com>
2024-09-22 14:05:58 +0300

    fix/added-bearer-to-request

--- src/utils/api/axiosInstance.js
+++ src/utils/api/axiosInstance.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'https://react-node-designer.glitch.me/api/v1',
+  baseURL: "https://react-node-designer.glitch.me/api/v1",
 });
 
 // Request interceptor
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
-      config.headers.Authorization = token;
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
@@ -25,9 +25,9 @@ axiosInstance.interceptors.response.use(
   },
   (error) => {
     if (error.response && error.response.status === 401) {
-      localStorage.removeItem('token');
-      localStorage.removeItem('isLoggedIn');
-      window.location.href = '/login';
+      localStorage.removeItem("token");
+      localStorage.removeItem("isLoggedIn");
+      window.location.href = "/login";
     }
     return Promise.reject(error);
   }
