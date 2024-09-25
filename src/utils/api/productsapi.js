8ad72b8ae858d993e7a65f2ea055c78acdf62a5b
farahmahfouz <farahmahfouz11@gmail.com>
2024-09-25 14:28:45 +0300

    Fix/searchbar

--- src/utils/api/productsapi.js
+++ src/utils/api/productsapi.js
@@ -1,9 +1,26 @@
+// import axios from "axios";
+
+// const API_URL = "https://react-node-designer.glitch.me/api/v1/products";
+
+// export const getAllProducts = async () => {
+//   return await axios.get(API_URL)
+//   .then( res => res.data.data.products)
+// };
+
+// export const getProductById = (id) => {
+//  return  axios.get(`${API_URL}/${id}`)
+//  .then( res => res.data.data.product)
+// };
+
 import axiosInstance from './axiosInstance';
 
-export const getAllProducts = async(searchTerm = '') => {
-  return await axiosInstance .get('/products', {
-    params: { search: searchTerm }, 
-  })
+export const getAllProducts = async () => {
+    return await axiosInstance.get("/products")
+    .then( res => res.data.data.products)
+  };
+
+export const searchProducts = async(searchTerm) => {
+  return await axiosInstance.get(`/products?search=${searchTerm}`)
     .then(res => res.data.data.products)
 };
 
