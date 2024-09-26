bf6fd58c6623dab6b6ba81ad6cc53032d6441fad
rwanadel <rwan32310@gmail.com>
2024-09-26 14:50:02 +0300

    feature/add-new-function-to-get-product

--- src/utils/api/productsapi.js
+++ src/utils/api/productsapi.js
@@ -1,15 +1,15 @@
-
-import axiosInstance from './axiosInstance';
+import axiosInstance from "./axiosInstance";
 
 export const getAllProducts = async () => {
-    return await axiosInstance.get("/products")
-    .then( res => res.data.data.products)
-  };
+  return await axiosInstance
+    .get("/products")
+    .then((res) => res.data.data.products);
+};
 
-export const searchProducts = async(searchTerm) => {
-  return await axiosInstance.get(`/products?search=${searchTerm}`)
-    .then(res => res.data.data.products)
-
+export const searchProducts = async (searchTerm) => {
+  return await axiosInstance
+    .get(`/products?search=${searchTerm}`)
+    .then((res) => res.data.data.products);
 };
 
 export const getProductById = (id) => {
@@ -22,6 +22,16 @@ export const getIsDesignableProduct = async () => {
   try {
     const res = await axiosInstance.get("/products/designable-products");
     return res.data.data.products; // Ensure this matches the response structure
+  } catch (error) {
+    console.error("Error fetching designable products:", error);
+    throw error; // Rethrow error for handling in your component
+  }
+};
+export const getIsDesignableProductById = async (id) => {
+  try {
+    const res = await axiosInstance.get(`/products/designable-products/${id}`);
+
+    return res.data.data.designableProduct; // Ensure this matches the response structure
   } catch (error) {
     console.error("Error fetching designable products:", error);
     throw error; // Rethrow error for handling in your component
