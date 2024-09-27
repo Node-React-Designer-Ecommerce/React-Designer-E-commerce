a8a61b74f34713d77cca87ef528c8c673364d8a7
yassersalem14 <salemyasserlallo1415@gmail.com>
2024-09-27 15:22:55 +0200

    Fix / products api file

--- src/utils/api/productsapi.js
+++ src/utils/api/productsapi.js
@@ -1,5 +1,7 @@
 import axiosInstance from "./axiosInstance";
 
+
+
 export const getAllProducts = async () => {
     const response = await axiosInstance.get(/products);
     return response.data.data.products;
