185d53a776d1cffa3af7489d0625244c0b4cc8b8
yassersalem14 <salemyasserlallo1415@gmail.com>
2024-10-26 20:04:45 +0200

    testing

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,6 +1,6 @@
 export default function Rating() {
     return (
-        <div className="rating">
+        <div className="rating invisible">
             <div className="rating">
                 <input type="radio" name="rating-2" className="mask mask-star-2 bg-amber-400 w-4 h-4" />
                 <input type="radio" name="rating-2" className="mask mask-star-2 bg-amber-400 w-4 h-4" defaultChecked />
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify"; // Import ToastContainer
+
 // import ProductsProvider from "./context/ProductsContext.jsx"
 
 createRoot(document.getElementById("root")).render(
@@ -10,8 +11,11 @@ createRoot(document.getElementById("root")).render(
   <>
     {/* <ProductsProvider> */}
     <App />
-    {/* </ProductsProvider> */}
-    <ToastContainer />
+    <ToastContainer
+      className="fixed top-16 right-4 z-50" // Updated for top-right positioning
+      toastClassName="bg-white bg-opacity-70 text-black rounded-lg shadow-lg p-4"
+      closeOnClick
+    />
   </>
   // </StrictMode>
 );
